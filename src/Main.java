cf14800630343e94cc00fb3c67a64ed4296bfd76
Senior-SergeantR <rabinotieno1@gmail.com>
2023-05-26 00:57:39 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Write a Java program to check if the given number is a prime number.
+
+        //Malyun
+
+        //write code here
+    }
+}
